76df79dea6e644623b8ea1f7281f32b53ea2e750
hossam hassan <hossamhassan@hossams-MacBook-Air.local>
2023-03-14 00:10:03 +0200

    save github api url to constant

--- app/services/github/repo_search_service.rb
+++ app/services/github/repo_search_service.rb
@@ -1,9 +1,11 @@
 require "ostruct"
 module Github
   class RepoSearchService
-    
+
+    GITHUB_ENDPOINT="https://api.github.com/search/repositories?q="
+
     def self.search(repo_name)
-      response = RestClient.get("https://api.github.com/search/repositories?q=#{repo_name}", headers={})
+      response = RestClient.get("#{GITHUB_ENDPOINT}#{repo_name}", headers={})
       if response.code == 200
        repos = JSON.parse(response.body)["items"]
        construct_repo_object(repos)
